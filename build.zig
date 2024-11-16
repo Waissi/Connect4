5531c4c971f538159e1ab41187e1a6506db9c2aa
Waissi <adr.adm@protonmail.com>
2024-11-16 19:25:32 +0100

    update build.zig

--- build.zig
+++ build.zig
@@ -5,17 +5,13 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const libraylib = b.dependency("libraylib", .{ .target = target, .optimize = optimize });
+    const raylib = libraylib.artifact("raylib");
+
     const exe = b.addExecutable(.{ .name = "Connect4", .target = target, .optimize = optimize });
     exe.addIncludePath(b.path("include"));
-    exe.addIncludePath(b.path("deps/raylib/zig-out/include"));
-    exe.addObjectFile(b.path("deps/raylib/zig-out/lib/libraylib.a"));
-    if (builtin.target.os.tag == .macos) {
-        exe.linkFramework("CoreVideo");
-        exe.linkFramework("IOKit");
-        exe.linkFramework("Cocoa");
-        exe.linkFramework("GLUT");
-        exe.linkFramework("OpenGL");
-    }
+    exe.addIncludePath(b.path("deps/raylib/src"));
+    exe.linkLibrary(raylib);
     exe.addCSourceFiles(.{ .files = &.{ "src/board.c", "src/game.c", "src/main.c" }, .flags = &.{} });
     exe.linkLibC();
     b.installArtifact(exe);
