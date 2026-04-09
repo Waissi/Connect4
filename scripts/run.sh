d6c7e9222f50e2fd7b35caaf253bcad1c2050780
Waissi <adr.adm@protonmail.com>
2026-04-09 21:23:51 +0200

    update gitignore

--- scripts/run.sh
+++
@@ -1,2 +0,0 @@
-clang -o Connect4 src/*.c -Iinclude -I/opt/local/include -L/opt/local/lib -lraylib -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL
-./Connect4
